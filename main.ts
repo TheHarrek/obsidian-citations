323b0cd0c19c0ce3b59e1fb77e279c42d4157a25
TheHarrek <harrex06@gmail.com>
2022-08-17 10:26:10 +1000

    First Full Release
    Finished writing version 1.0.0.

--- main.ts
+++ main.ts
@@ -1,137 +1,97 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, Plugin, Modal, Setting, MarkdownView, moment } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+// huetoansu
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+class globals {
+  cursor: any;
+};
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export class modal extends Modal {
+  constructor(app: App, title: string) {
+    super(app);
+    title = title;
+  }
 
-	async onload() {
-		await this.loadSettings();
+  onOpen() {
+    const { contentEl } = this;
+    contentEl.createEl("h1", { text: "Citation" });
+    new Setting(contentEl)
+      .setName("Source")
+      .addText((text) =>
+        text.onChange((value) => {
+          this.source = value
+        }));
+    new Setting(contentEl)
+      .setName("Title")
+      .addText((text) =>
+        text.onChange((value) => {
+          this.title = value
+        }));
+    new Setting(contentEl)
+      .setName("Authors")
+      .addText((text) =>
+        text.onChange((value) => {
+          this.author = value
+        }));
+    new Setting(contentEl)
+      .setName("Date Published")
+      .addText((text) =>
+        text.onChange((value) => {
+          this.date = value
+        }));
+    new Setting(contentEl)
+    .addButton((btn) =>
+      btn
+        .setButtonText("Auto")
+        .setCta()
+        .onClick(() => {
+          this.dateViewed = moment().format("DD-MM-YYYY"); // Needs to set to today
+      }))
+      .setName("Date Viewed")
+      .addText((text) =>
+        text.onChange((value) => {
+          this.dateViewed = value
+        }));
+    new Setting(contentEl)
+      .addButton((btn) =>
+        btn
+          .setButtonText("Submit")
+          .setCta()
+          .onClick(() => {
+            this.close();
+            this.onSubmit(this)
+          }));
+  }
+  onClose() {
+    let { contentEl } = this;
+    contentEl.empty();
+  }
+  onSubmit(a) {
+    new Notice("Citation Added");
+    const editor = this.app.workspace.getActiveViewOfType(MarkdownView).editor;
+    editor.replaceRange((`${a.author} ${a.date}, ${a.title}, Viewed ${a.dateViewed}, ${a.source}`), globals.cursor);
+  }
+  }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+export default class ExamplePlugin extends Plugin {
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+  async onload() {
+    this.addCommand({
+      id: "add-citation",
+      name: "Add Citation",
+      editorCallback: (editor: Editor) => {
+        globals.cursor = editor.getCursor();
+        new modal(this.app).open();
+      }
+    });
+    
+  }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+  async onunload() {
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  }
 }
